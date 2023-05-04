b4cd23c3d71613631ed0536c524b9c926c8006d4
NGAM TARYLL <taryllngam@gmail.com>
2023-05-04 14:08:41 +0100

    linter corrections

--- script.js
+++ script.js
@@ -1,75 +1,99 @@
-const inputField = document.getElementById("input");
-const addButton = document.getElementById("btn");
-const editBtn = document.getElementById("edit");
-const deleteBtn = document.getElementById("delete");
-const list = document.getElementById("item-list");
+const inputField = document.getElementById('input')
+const addButton = document.getElementById('btn')
+const deleteBtn = document.getElementById('delete')
+const list = document.getElementById('item-list')
 
-let arrTask = [];
+let arrTask = [
+]
 
-const taskTemplate = () => {
-  return arrTask.map(
-    (work) => `
-    <li class="list">
-        <!-- <label for="1"> -->
-            <!-- <input type="checkbox" id="1"> -->
-            <p> <input type="checkbox" id="1"> ${work.task} from ${work.time} - 08:30</p>
-        <!-- </label> -->
-        <div class="settings">
-        <i class="fa fa-ellipsis-h" aria-hidden="true" id="bars"></i>
-        <ul class="task">
-            <li class="controls">
-                <i class="fa fa-pencil" aria-hidden="true" id="edit"></i>
-                Edit
-            </li>
-            <li
-                class="controls"
-                onclick="deleteTask(${work.id})"
-            >
-                <i class="fa fa-trash" aria-hidden="true" id="delete"></i>
-                Delete
-            </li>
-        </ul>
-        </div>
-    </li>`
-  );
-};
+let taskHolder = {}
+
+const displayTask = (newItem) => {
+  const newList = document.createElement('li')
+  newList.classList.add('list')
+  newList.id = newItem.id
+
+  newList.innerHTML = `
+    <p name=${newItem.id} > <input type="checkbox"> ${newItem.task}</p>
+    <div class="settings">
+    <i class="fa fa-ellipsis-h" aria-hidden="true" id="bars"></i>
+    <ul class="task">
+        <li 
+        class="controls"
+        onclick="editTask(${newItem.id})"
+        >
+            <i class="fa fa-pencil" aria-hidden="true" id="edit"></i>
+            Edit
+        </li>
+        <li
+            class="controls"
+            onclick="deleteTask(${newItem.id})"
+        >
+            <i class="fa fa-trash" aria-hidden="true" id="delete"></i>
+            Delete
+        </li>
+    </ul>
+    </div>
+  `
+  list.appendChild(newList)
+}
 
 const addItem = () => {
   if (inputField.value <= 0) {
-    alert("Please add an item");
-    return;
+    alert('Please add an item')
+    return
   }
 
-  let newId = 0;
+  if (taskHolder.id) {
+    const [pTagToEdit] = document.getElementsByName(taskHolder.id)
+
+    pTagToEdit.innerHTML = `
+     <input type="checkbox"> ${inputField.value}
+    `
+    inputField.value = ''
+    taskHolder = {}
+    return
+  }
+
+  let newId = 0
   if (arrTask.length <= 0) {
-    newId = 1;
+    newId = 1
   } else {
-    newId = arrTask[arrTask.length - 1].id + 1;
+    newId = arrTask[arrTask.length - 1].id + 1
   }
 
   const newItem = {
     id: newId,
-    task: inputField.value,
-    time: new Date().toLocaleTimeString(),
-  };
+    task: inputField.value
+  }
 
-  arrTask.push(newItem);
-  inputField.value = "";
-  list.innerHTML = taskTemplate(arrTask);
-};
+  arrTask.push(newItem)
+  inputField.value = ''
+  displayTask(newItem)
+}
 
-const editTask = (id) => {
-    //
+const editTask = (name) => {
+  const [pTagToEdit] = document.getElementsByName(name)
+  const [taskToEdit] = arrTask.filter((item) => {
+    return item.id === name
+  })
+  inputField.value = pTagToEdit.innerText
+  taskHolder = taskToEdit
+  console.log(taskToEdit, pTagToEdit)
 }
 
 const deleteTask = (id) => {
-    const newArr = arrTask.filter((item) => {
-        return item.id !== id;
-    });
+  document.getElementById(id).remove()
 
-    arrTask = newArr
+  const newArr = arrTask.filter((item) => {
+    return item.id !== id
+  })
 
-    list.innerHTML = taskTemplate(newArr);
+  arrTask = newArr
 }
 
-addButton.addEventListener("click", addItem);
+addButton.addEventListener('click', addItem)
+
+function saveData () {
+  localStorage.setItem('data', list.innerHTML)
+}
